2e59840c0ef28d0ab320b69366ca748199d6b491
Korbinian Riedhammer <korbinianr@gmail.com>
2018-04-18 17:44:01 +0200

    typo

--- src/main/java/com/github/sikoried/fjama/Matrix.java
+++ src/main/java/com/github/sikoried/fjama/Matrix.java
@@ -1,14 +1,15 @@
 package com.github.sikoried.fjama;
 
+import com.github.sikoried.fjama.util.Maths;
+
 import java.text.NumberFormat;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.util.Locale;
-import java.text.FieldPosition;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.StreamTokenizer;
-import Jama.util.*;
+
 
 /**
    Jama = Java Matrix class.
